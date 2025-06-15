99023ae16c53bca555c32fdcc33c7febabcc01cf
VedeVpered <dreamway.agency01@gmail.com>
2025-06-16 02:27:31 +0300

    перезалил

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Дозволити всі фронтенди (можна обмежити потім)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -17,7 +16,7 @@ def root():
     return {"message": "DreamPost backend active"}
 
 @app.post("/publish")
-async def handle_publish(req: Request):
+async def publish(req: Request):
     data = await req.json()
     print(">>> Отримано:", data)
     return {"message": "✅ Прийнято"}
